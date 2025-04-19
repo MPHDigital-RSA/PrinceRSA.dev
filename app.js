e66f9669bbed07b5f3bd67342861fe0c97f0bf78
Prince Hlela <mphdigital@protonmail.com>
2025-04-19 18:05:58 +0200

    the hero section

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const header = document.querySelector('.header');
 const menuToggler = document.querySelector('.menu');
+const logoLink = document.querySelector('.logo');
+const headerbutton = document.querySelector('.header-button');
+const navLinks = document.querySelectorAll('.nav-link')
 
 
+//toggle the mobile nav menu
 menuToggler.addEventListener('click', () => {
     header.classList.toggle("mobile")
 })
+
+// close the menu when any nav link in the header is clicked
+navLinks.forEach((link) => {
+    link.addEventListener('click', () =>{
+        closeMobileNav();
+    })
+})
+
+logoLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    closeMobileNav();
+})
+
+headerbutton.addEventListener('click', () => {
+    closeMobileNav();
+})
+
+
+
+
+// functions------------------------------------
+
+function closeMobileNav (){
+header.classList.remove('mobile');
+}
