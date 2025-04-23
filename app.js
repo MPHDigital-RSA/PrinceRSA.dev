233582ae83b4bb94641ad69a623f848e65cdb44c
Prince Hlela <mphdigital@protonmail.com>
2025-04-23 21:15:33 +0200

    refactor: clean up navigation menu styles and improve mobile responsiveness

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const header = document.querySelector('.header');
 const menuToggler = document.querySelector('.menu');
 const logoLink = document.querySelector('.logo');
 const headerbutton = document.querySelector('.header-button');
-const navLinks = document.querySelectorAll('.nav-link')
+const navLinks = document.querySelectorAll('.nav-link');
+
+
+// window.addEventListener('resize', function() {
+//     if(window.innerWidth = 1200){
+//         header.classList.toggle("mobile");
+//     }
+// });
 
 
 //toggle the mobile nav menu
 menuToggler.addEventListener('click', () => {
     header.classList.toggle("mobile")
+
 })
 
 // close the menu when any nav link in the header is clicked
